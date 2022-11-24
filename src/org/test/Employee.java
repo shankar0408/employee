c2e9be603b072be41c0d68e30950a63030c5e9b7
shankar0408 <shankarmp04@gmail.com>
2022-11-24 10:29:22 +0530

    added the employee details

---
+++ src/org/test/Employee.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Employee {
+	
+	private void employeename() {
+		System.out.println("name of employee : karan");
+
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
